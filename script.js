052c7ccf81a785a53abf5f709c8c17d59d605b8c
AntonLevchenko <33231038+AntonLevchenko@users.noreply.github.com>
2017-10-31 23:40:15 +0200

    site

---
+++ script.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+	$("#header").removeClass('default');
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 20) {
+			$("#header").addClass("default").fadeIn('fast');
+		} else {
+			$("#header").removeClass("default").fadeIn('fast');
+		};
+	});
+});
+
+$(function () {
+	var elWrap = $('#about_us_slider'),
+		el =  elWrap.find('img'),
+		indexImg = 1,
+		indexMax = el.length,
+		phase = 3000;
+	
+	function change () {
+		el.fadeOut(500);
+		el.filter(':nth-child('+indexImg+')').fadeIn(500);
+	}	
+		
+	function autoCange () {	
+		indexImg++;
+		if(indexImg > indexMax) {
+			indexImg = 1;
+		}			
+		change ();
+	}	
+	var interval = setInterval(autoCange, phase);
+
+	elWrap.mouseover(function() {
+		clearInterval(interval);
+	});
+	elWrap.mouseout(function() {
+		interval = setInterval(autoCange, phase);
+	});
+	
+	elWrap.append('<span class="next"></span><span class="prev"></span>');
+	var	btnNext = $('span.next'),
+		btnPrev = $('span.prev');
+		
+	btnNext.click(function() {
+		indexImg++;
+		if(indexImg > indexMax) {
+			indexImg = 1;
+		}
+		change ();
+	});
+	btnPrev.click(function() {
+		indexImg--;
+		if(indexImg < 1) {
+			indexImg = indexMax;
+		}
+		change ();
+	});	
+});
